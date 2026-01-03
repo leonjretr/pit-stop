2fe5f46a74c42e4d509a7a5088b107971214987a
Leon <leo.muscle28@gmail.com>
2026-01-03 22:06:59 +0000

    Added: new header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,12 +5,15 @@ const Header = () => {
     
     return (
         <div className={"flex justify-between items-center w-full h-20 p-3 bg-brand-lightgold text-brand-coffee font-poppins font-bold"}>
-            <div className={"w-4"}>
-                <Image className={"flex w-[400px] h-[100px]"} src={"/images/portafilter.png"} alt={"Portafilter"}
-                height={"500"} width={"500"}/>
-                pit stop
+            <div className={"flex gap-x-1"}>
+                <div className={"flex w-full p-0"}>
+                    <Image src={"/images/portafilter.png"} alt={"Portafilter"}
+                           height={"50"} width={"50"}/>
+                </div>
+                <h1 className={"font-poppins text-xl p-0"}>
+                    pit stop
+                </h1>
             </div>
-
         </div>
     );
 };
