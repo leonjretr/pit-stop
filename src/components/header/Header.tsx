1c53d5574a34ea0207288b410326d3a970db811f
Leon <leo.muscle28@gmail.com>
2026-01-03 21:28:32 +0000

    Added: new header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Image from "next/image";
 
 const Header = () => {
+    
     return (
-        <div className={"flex justify-between w-full h-20 p-5 bg-brand-lightgold text-brand-coffee font-poppins font-bold"}>
+        <div className={"flex justify-between items-center w-full h-20 p-3 bg-brand-lightgold text-brand-coffee font-poppins font-bold"}>
             <div className={"w-4"}>
+                <Image className={"flex w-[400px] h-[100px]"} src={"/images/portafilter.png"} alt={"Portafilter"}
+                height={"500"} width={"500"}/>
                 pit stop
             </div>
 
