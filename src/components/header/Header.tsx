1008a381b50ce6c1507f96d114fe6ac457a8207f
Leon <leo.muscle28@gmail.com>
2026-01-08 19:00:40 +0000

    Added: changes to Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,19 +4,16 @@ const Header = () => {
 
     return (
         <div
-            className={"flex justify-center w-full p-3 bg-white text-brand-coffee border-b border-solid border-black"}>
+            className={"flex justify-center w-full p-4 bg-white text-brand-coffee border-b-2 border-solid border-brand-coffee"}>
             <button className={"flex items-center"}>
-                {/*<Image src={"/images/portafilter.png"} alt={"Portafilter"}*/}
-                {/*       height={40} width={40}/>*/}
                 <div className={"flex flex-col justify-center"}>
                     <div className={"flex text-start font-poppins font-bold text-3xl p-2 leading-0.5"}>
                         things talk
                     </div>
                     <div className={"flex justify-center font-poppins italic text-xs pt-3 leading-[0.75]"}>
-                        and what do they say?
+                        what&#39;s their language?
                     </div>
                 </div>
-
             </button>
         </div>
     );
