658a5efaba2e3dc94e8a144733f2acb4479f85e1
Leon <leo.muscle28@gmail.com>
2026-01-10 22:27:20 +0000

    Added: changes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
+import {IoAdd} from "react-icons/io5";
+
 export default function Home() {
     return (
         <div className={"min-h-screen bg-white"}>
             <div className={"flex justify-center p-5"}>
                 <div className={"flex items-center w-80 h-14 gap-3 border-2 border-brand-walnut p-2 font-nunito "}>
-                    <div className={"flex items-center justify-center w-12 h-10 border-2 border-brand-coffee font-bold"}>
-                        +
+                    <div
+                        className={"flex items-center justify-center w-12 h-10 border-2 border-brand-coffee font-bold"}>
+                        <IoAdd/>
                     </div>
                     <div className={"font-semibold"}>
                         Add a new object memory
                     </div>
                 </div>
             </div>
-
         </div>
     );
 }
