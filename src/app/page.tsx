b10896aa3a7ae05544f045f850320640856f6a1d
Leon <leo.muscle28@gmail.com>
2026-01-11 23:23:39 +0000

    Added: changes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
+import ObjectMemoryCard from "@/components/cards/ObjectMemoryCard";
 import {IoAdd} from "react-icons/io5";
 
 export default function Home() {
     return (
         <div className={"min-h-screen bg-white"}>
-            <div className={"flex justify-center p-5"}>
-                <div className={"flex items-center w-80 h-14 gap-3 border-2 border-brand-walnut p-2 font-nunito rounded-lg"}>
+            <div className={"flex flex-col items-center justify-center p-5 gap-y-5"}>
+                <div className={"flex items-center w-80 h-14 gap-3 border-x-2 border-brand-walnut p-2 font-nunito"}>
                     <button
-                        className={"flex items-center justify-center w-12 hover:scale-110 hover:bg-neutral-100 active:scale-95 transition-transform cursor-pointer h-10 border-2 border-brand-coffee font-bold rounded-lg"}>
+                        className={"flex items-center justify-center w-12 hover:scale-110 hover:bg-neutral-100 active:scale-95 transition-transform cursor-pointer h-10 border-2 border-brand-walnut font-bold rounded-lg"}>
                         <IoAdd className={"text-3xl"}/>
                     </button>
                     <div className={"font-semibold"}>
                         Add a new object memory
                     </div>
                 </div>
+                <ObjectMemoryCard/>
             </div>
         </div>
     );
---
+++ src/components/cards/ObjectMemoryCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {IoAdd} from "react-icons/io5";
+
+const ObjectMemoryCard = () => {
+    return (
+        <div className={"flex items-center w-96 h-14 gap-3 border-x-2 border-brand-walnut p-2 font-nunito"}>
+            <div className={"font-semibold"}>
+                My first memory
+            </div>
+        </div>
+    );
+};
+
+export default ObjectMemoryCard;
