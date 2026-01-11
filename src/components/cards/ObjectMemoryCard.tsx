9b08cc56ba011442c0bdc7eb4e0819f63315235f
Leon <leo.muscle28@gmail.com>
2026-01-11 23:30:19 +0000

    Added: changes to ObjectMemoryCard.tsx

--- src/components/cards/ObjectMemoryCard.tsx
+++ src/components/cards/ObjectMemoryCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {IoAdd} from "react-icons/io5";
 
 const ObjectMemoryCard = () => {
     return (
-        <div className={"flex items-center w-96 h-14 gap-3 border-x-2 border-brand-walnut p-2 font-nunito"}>
-            <div className={"font-semibold"}>
+        <div className={"flex flex-col justify-center w-96 h-16 gap-3 border-y-2 hover:border-x-2 hover:border-y-0 border-brand-walnut p-2 font-nunito"}>
+            <div className={"font-semibold text-lg"}>
                 My first memory
+            </div>
+            <div className={"text-sm"}>
+                Description of the first memory!
             </div>
         </div>
     );
