9d36ecf02e56edc02de5d4fc5efa3302218011af
Leon <leo.muscle28@gmail.com>
2026-01-12 18:35:38 +0000

    Added: small hover card added

--- src/components/cards/AddMemoryCard.tsx
+++ src/components/cards/AddMemoryCard.tsx
@@ -2,16 +2,36 @@
 import {motion} from 'motion/react';
 import React from 'react';
 import {IoAdd} from "react-icons/io5";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 const AddMemoryCard = () => {
     return (
         <div className={"flex items-center w-80 gap-x-3  p-2 font-nunito"}>
-            <motion.button
-                whileHover={{scale: 1.1}}
-                whileTap={{scale: 0.95}}
-                className={"flex items-center justify-center w-12 cursor-pointer h-10 border-2 border-brand-walnut font-bold rounded-lg"}>
-                <IoAdd className={"text-3xl"}/>
-            </motion.button>
+            <HoverCard>
+                <HoverCardTrigger asChild>
+                    <motion.button
+                        whileHover={{scale: 1.1}}
+                        whileTap={{scale: 0.95}}
+                        className={"flex items-center justify-center w-12 cursor-pointer h-10 border-2 border-brand-walnut font-bold rounded-lg"}>
+                        <IoAdd className={"text-3xl"}/>
+                    </motion.button>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                    <div className={"flex flex-col font-poppins"}>
+                        <div className={"font-bold text-md"}>
+                            Adding a new memory
+                        </div>
+                        <div className={"text-sm"}>
+                            Tell a story of your object: whether it&#39;s a pen gifted by your father, or a camera you bought last week - doesn&#39;t matter. Any story is great.
+                        </div>
+                    </div>
+
+                </HoverCardContent>
+            </HoverCard>
             <div className={"font-semibold"}>
                 Add a new object memory
             </div>
--- src/components/cards/ObjectMemoryCard.tsx
+++ src/components/cards/ObjectMemoryCard.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const ObjectMemoryCard = () => {
     return (
         <motion.div
-            whileHover={{scale:1.15}}
+            whileHover={{scale:1.1}}
             className={"flex flex-col justify-center cursor-pointer w-96 gap-3 border-2 rounded-lg border-brand-walnut p-2 font-nunito"}>
             <div className={"font-semibold text-lg"}>
                 My first memory
