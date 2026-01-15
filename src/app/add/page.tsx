f91065918ea099f178112c225ded69cfd6be2870
Leon <leo.muscle28@gmail.com>
2026-01-15 18:47:42 +0000

    Added: new pages, AddMemoryButton.tsx

---
+++ src/app/add/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {IoAdd} from "react-icons/io5";
+import {motion} from "motion/react";
+
+const Page = () => {
+    return (
+        <div className={"min-h-screen bg-white overflow-visible"}>
+
+
+        </div>
+    );
+};
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import AddMemoryCard from "@/components/cards/AddMemoryCard";
+import AddMemoryButton from "@/components/buttons/AddMemoryButton";
 import ObjectMemoryCard from "@/components/cards/ObjectMemoryCard";
 
 export default function Home() {
     return (
         <div className={"min-h-screen bg-white overflow-visible"}>
             <div className={"flex flex-col items-center justify-center p-5 gap-y-5"}>
-                <AddMemoryCard/>
+                <AddMemoryButton/>
                 <ObjectMemoryCard/>
             </div>
         </div>
